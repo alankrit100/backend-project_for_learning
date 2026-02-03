468ada9b7222a4c2b82fae72c1ab7cb1e198194d
Alankrit Bajoria <alankritbajoria100@gmail.com>
2026-02-03 17:30:19 +0530

    MongoDb Connected

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { configDotenv } from "dotenv";
+import connectDB from "./db/index.js";
+
+configDotenv({
+  path: "./.env",
+});
+
+connectDB();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", () => {
+//             console.log("ERROR: ", error);
+//             throw error            
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         }
+//         )
+//     } catch (error) {
+//         console.error("ERROR: ", error);
+//         throw err
+        
+//     }
+// }) ();
